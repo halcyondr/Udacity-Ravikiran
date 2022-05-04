65b7be470d2f4b5f23456a379e58575f7506b45f
halcyondr <53500920+halcyondr@users.noreply.github.com>
2022-05-04 14:46:00 -0400

    Update ec2.tf

--- project/starter-code/zone1/ec2.tf
+++ project/starter-code/zone1/ec2.tf
@@ -3,7 +3,7 @@
    instance_count     = var.instance_count
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
-   aws_ami            = "ami-04ff923e070e28b56"
+   aws_ami            = "ami-095a421b30f14e55f"
    private_subnet_ids = module.vpc.private_subnet_ids
    public_subnet_ids  = module.vpc.public_subnet_ids
    vpc_id             = module.vpc.vpc_id
