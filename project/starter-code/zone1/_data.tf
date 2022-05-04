a32b5338acf99c4b9f4496a1521c8f08c7fc0dca
halcyondr <53500920+halcyondr@users.noreply.github.com>
2022-05-04 14:44:58 -0400

    Update _data.tf

--- project/starter-code/zone1/_data.tf
+++ project/starter-code/zone1/_data.tf
@@ -2,7 +2,7 @@ data "aws_caller_identity" "current" {}
 
  data "aws_ami" "amazon_linux_2" {
    most_recent = true
-   owners      = ["amazon"]
+   owners      = ["313242119113"]
 
    filter {
      name   = "owner-alias"
